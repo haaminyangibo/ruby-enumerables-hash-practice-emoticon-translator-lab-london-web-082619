af98a8152773acea95d3417708c0c2d4c16586de
Haami Nyangibo <haaminyangibo@gmail.com>
2019-08-20 14:10:29 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,16 +1,4 @@
-# require modules here
 
-def load_library
-  # code goes here
-end
-
-def get_japanese_emoticon
-  # code goes here
-end
-
-def get_english_meaning
-  # code goes here
-end
 
 require 'yaml'
 require 'pry'
